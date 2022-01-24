b725f161d16775fb972460e1d0c33975ef7d33e0
moh-diab <mohdiab@gmail.com>
2022-01-23 22:21:25 -0500

    Transforming Data with Selectors

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,8 @@ import DayListItem from "components/DayListItem";
 
 export default function Daylist({days, selectedDay, setDay} = this.props) {
   
-  console.log("this is the day", days);
+  //console.log("this is the day", days);
+  
   return (
     <ul>
     {days.map(day => (
---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+
+
+
+export function getAppointmentsForDay(state, day) {
+  //return an array of appointments for that day
+  
+  if(!state.days){
+    return [];
+  }
+
+  let currentDay = state.days.filter(obj => obj.name === day)[0];
+  if(!currentDay){
+    return [];
+  }
+
+  let apptArray = [];
+  for (const id of currentDay.appointments) {
+    const existingAppt = state.appointments[id];
+    apptArray.push(existingAppt);
+  }
+  return apptArray;
+}
